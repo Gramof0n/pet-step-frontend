fc70a18fe5e67b3c5bbbe98fae4530ee06520c8a
Nikola Milovanovic <nikola.milovanovic@student.um.si>
2021-06-08 20:24:45 +0200

    gotovo

--- screens/WalkHistory.tsx
+++ screens/WalkHistory.tsx
@@ -1,46 +1,49 @@
 import Walks from "../components/WalkComponent";
-import React, { useEffect, useState } from 'react';
-import { Walks_type } from "../types"
+import React, { useCallback, useEffect, useState } from "react";
+import { Walks_type } from "../types";
 import { View, StatusBar, StyleSheet, ActivityIndicator } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { get } from '../utils/apiCalls';
+import { get } from "../utils/apiCalls";
+import { useFocusEffect } from "@react-navigation/core";
 
 const WalkHistory = () => {
-
   const [walks, setWalks] = useState<Array<Walks_type>>([]);
-  const [loading, setLoading] = useState<boolean>(true)
+  const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    fetchAllWalks();
-  }, [])
+  useFocusEffect(
+    useCallback(() => {
+      fetchAllWalks();
+    }, [])
+  );
 
   const fetchAllWalks = async () => {
-    const userFromStorage = await AsyncStorage.getItem('loggedUser')
+    const userFromStorage = await AsyncStorage.getItem("loggedUser");
     //console.log(userFromStorage)
-    const id = JSON.parse(userFromStorage!).id
+    const id = JSON.parse(userFromStorage!).id;
     //console.log(id)
-    const res = await get(`users/get-one/${id}`) 
-    setWalks(res?.data.walks)
+    const res = await get(`users/get-one/${id}`);
+    setWalks(res?.data.walks);
     setLoading(false);
-  }
+  };
   return (
     <View style={styles.container}>
       <ScrollView style={styles.scrollView}>
-        {
-          loading ? <ActivityIndicator 
-          size= {30}
-          color= "#5E73BD" /> 
-          : walks.map((walk, index) => {
-            return <Walks 
-            key={index}
-            distance={walk.distance}
-            date={walk.date}
-            encoded_polyline={walk.encoded_polyline}
-            time={walk.time}
-            />
+        {loading ? (
+          <ActivityIndicator size={30} color="#5E73BD" />
+        ) : (
+          walks.map((walk, index) => {
+            return (
+              <Walks
+                key={index}
+                distance={walk.distance}
+                date={walk.date}
+                encoded_polyline={walk.encoded_polyline}
+                time={walk.time}
+              />
+            );
           })
-      }
+        )}
       </ScrollView>
     </View>
   );
@@ -54,6 +57,6 @@ const styles = StyleSheet.create({
   scrollView: {
     marginHorizontal: 22,
   },
-})
+});
 
 export default WalkHistory;
